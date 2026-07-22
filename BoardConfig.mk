2811fae3397c471ba89c7f991ede3e907df2f96e
Mikhail Mlhcnn <mikhail@advnirr.org>
2026-07-22 16:33:15 +0300

    degas: add device compatibility matrix for aidl.mtdservice
    HyperOS renamed the HAL package to vendor.xiaomi.hardware.aidl.mtdservice;
    the hardware/xiaomi matrix only lists the old vendor.xiaomi.hardware.mtdservice,
    so check_vintf fails.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,8 @@ TARGET_SCREEN_DENSITY := 480
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    $(DEVICE_PATH)/configs/vintf/degas_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
